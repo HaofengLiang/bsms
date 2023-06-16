42db8e989692ca97ceb78faf5e1eb830f89eec9f
HaofengLiang <haofengliang.developer@gmail.com>
2023-06-15 22:23:25 -0400

    Add app bar.

--- web/app/page.js
+++ web/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ResponsiveAppBar from "@/components/appBar";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello, Next.js!</h1>
-    </main>
+    <>
+      <ResponsiveAppBar />
+      <main className={styles.main}>
+        <h1>Hello, Next.js!</h1>
+      </main>
+    </>
   );
 }
